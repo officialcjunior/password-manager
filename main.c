1e3f03d428e5f17149bf014a80305d1c53c51447
officialcjunior <realc@protonmail.com>
2020-04-23 19:58:00 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "manage.h"
+
+int main()
+{
+	int choice;
+	puts("Welcome to cjunior's password manager");
+	do
+	{
+		puts("1. Save a new password\n2. Open a saved password\n3.Exit");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:save();break;
+			case 2:open();break;
+			default: puts("Enter a valid choice");break;
+		}
+	}
+	while (choice!=3);
+	return 0;
+}
